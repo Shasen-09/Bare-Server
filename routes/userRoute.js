5e109c0e1894ffd76c7119de7337bcc906fc5741
Shasen Shrestha <shasenshrestha3@gmail.com>
2025-12-24 13:34:31 +0545

    Route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,9 +4,9 @@ const { addUser, getUser } = require('../controller/userController.js');
 const { login, refresh } = require('../controller/authController.js');
 const authenticate = require('../middlewares/authMiddleware.js');
 
-router.post('/adduser', authenticate(addUser))
-router.get('/getuser', (getUser))
+router.post('/adduser', authenticate, addUser)
+router.get('/getuser', authenticate, getUser)
 router.post('/login', login)
-router.post('/refresh', authenticate(refresh))
+router.post('/refresh', authenticate, refresh)
 
 module.exports = router;
